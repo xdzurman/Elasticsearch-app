831730142dd7cdb6da7a9275f3a21fa8d7b9424e
xdzurman <kamil.dzurman@gmail.com>
2019-10-25 11:00:25 +0200

    Add fuzziness and cleanup

--- src/SearchField.js
+++ src/SearchField.js
@@ -1,283 +1,238 @@
 import React from 'react';
 
 class SearchField extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            query: '',
-            fuzzy: false,
-            strengthLow: '',
-            strengthHigh: '',
-            lossesLow: '',
-            lossesHigh: '',
-            yearLow: '',
-            yearHigh: '',
-            sort: 'score',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      fuzzy: false,
+      strengthLow: '',
+      strengthHigh: '',
+      lossesLow: '',
+      lossesHigh: '',
+      yearLow: '',
+      yearHigh: '',
+      sort: 'score',
+    };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleCheckbox = this.handleCheckbox.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.appendRangePairQuery = this.appendRangePairQuery.bind(this);
-        this.appendRangeSingleQuery = this.appendRangeSingleQuery.bind(this);
-        this.getSortQuery = this.getSortQuery.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.appendRangePairQuery = this.appendRangePairQuery.bind(this);
+    this.appendRangeSingleQuery = this.appendRangeSingleQuery.bind(this);
+    this.getSortQuery = this.getSortQuery.bind(this);
+  }
+
+  handleCheckbox(e) {
+    this.setState({ [e.target.name]: e.target.checked })
+  }
+
+  handleInputChange(e) {
+    console.log(e.target.name, e.target.value)
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  getSortQuery(type) {
+    if (type === 'score') {
+      return '_score'
+    } else if (type === 'year') {
+      return { year: 'desc' }
     }
+  }
 
-    handleCheckbox(e) {
-        this.setState({[e.target.name]: e.target.checked})
-    }
+  appendRangeSingleQuery(low, high, property) {
+    return low !== '' && high !== '' ? {
+      range: {
+        [property]: {
+          gte: low,
+          lte: high
+        }
+      }
+    } : {}
+  }
 
-    handleInputChange(e) {
-      console.log(e.target.name, e.target.value)
-        this.setState({[e.target.name]: e.target.value});
-    }
+  appendRangePairQuery(low, high, property1, property2) {
+    return low !== '' && high !== '' ? {
+      bool: {
+        should: [
+          this.appendRangeSingleQuery(low, high, property1),
+          this.appendRangeSingleQuery(low, high, property2)
+        ]
+      }
+    } : {}
+  }
 
-    getSortQuery(type) {
-      if (type === 'score') {
-        return '_score'
-      } else if (type === 'year') {
-        return { year: 'desc' }
+  async handleSubmit(e) {
+    e.preventDefault()
+
+    const queryBody = {
+      bool: {
+        must: [
+          {
+            multi_match: {
+              query: this.state.query,
+              fields: [
+                "title^5",
+                "text",
+                "belligerents1^3",
+                "belligerents2^3",
+                "leaders1^3",
+                "leaders2^3"
+              ],
+              fuzziness : this.state.fuzzy ? "AUTO" : undefined
+            }
+          },
+          this.appendRangePairQuery(this.state.strengthLow, this.state.strengthHigh,
+            'strength1', 'strength2'),
+          this.appendRangePairQuery(this.state.lossesLow, this.state.lossesHigh,
+            'losses1', 'losses2'),
+          this.appendRangeSingleQuery(this.state.yearLow, this.state.yearHigh,
+            'year'),
+        ]
       }
     }
 
-    appendRangeSingleQuery(low, high, property) {
-      return low !== '' && high !== '' ? {
-        bool: {
-          must: [
-            {
-              range: {
-                [property]: {
-                  gte: low
-                }
-              }
-            },
-            {
-              range: {
-                [property]: {
-                  lte: high
-                }
-              }
-            }
-          ]
-        }
-      } : {}
-    }
-
-    appendRangePairQuery(low, high, property1, property2) {
-      return low !== '' && high !== '' ? {
-        "bool": {
-          "must": [
-            {
-              "bool": {
-                "should": [
-                  {
-                    "range": {
-                      [property1]: {
-                        "gte": low
-                      }
-                    }
-                  },
-                  {
-                    "range": {
-                      [property2]: {
-                        "gte": low
-                      }
-                    }
-                  }
-                ]
-              }
-            },
-            {
-              "bool": {
-                "should": [
-                  {
-                    "range": {
-                      [property1]: {
-                        "lte": high
-                      }
-                    }
-                  },
-                  {
-                    "range": {
-                      [property2]: {
-                        "lte": high
-                      }
-                    }
-                  }
-                ]
-              }
-            }
-          ]
-        }
-      } : {}
-    }
-
-    async handleSubmit(e) {
-        e.preventDefault()
-
-        const req = {
-            sort: this.getSortQuery(this.state.sort),
-            "query": {
-              "bool": {
-                "must": [
-                  {
-                    "multi_match": {
-                      "query": this.state.query,
-                      "fields": [
-                        "title^5",
-                        "text",
-                        "belligerents1^3",
-                        "belligerents2^3",
-                        "leaders1^3",
-                        "leaders2^3"
-                      ]
-                    }
-                  },
-                  this.appendRangePairQuery(this.state.strengthLow, this.state.strengthHigh,
-                    'strength1', 'strength2'),
-                  this.appendRangePairQuery(this.state.lossesLow, this.state.lossesHigh,
-                    'losses1', 'losses2'),
-                  this.appendRangeSingleQuery(this.state.yearLow, this.state.yearHigh,
-                    'year'),
-                ] 
-              }
-            },
-            "highlight": {
-              "fields": {
-                "title": {}
-              }
-            }
+    const req = {
+      sort: this.getSortQuery(this.state.sort),
+      query: queryBody,
+      highlight: {
+        fields: {
+          text: {
+            pre_tags: ['<b>'],
+            post_tags: ['</b>']
           }
-        
-        const cleanedReq = JSON.stringify(req).replace(/,{}/g, '')
-        // console.log(JSON.stringify(req))
-        console.log(JSON.stringify(req).replace(/,{}/g, ''))
-
-        const data = await fetch('/battles/_search', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: cleanedReq
-        }).then(res => res.json()).catch(e => console.log(e))
-        this.props.fillResults(data)
+        }
+      }
     }
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <span className="describe"> Search Query </span>
-                        <input 
-                            name="query"
-                            type="text"
-                            value={this.state.query}
-                            onChange={this.handleInputChange} 
-                            placeholder="Query text..."
-                        />
-                    </label>
+    const cleanedReq = JSON.stringify(req).replace(/,{}/g, '')
+    console.log(JSON.stringify(req).replace(/,{}/g, ''))
 
-                    <label>
-                        <span className="describe"> Allow fuzzy search </span>
-                        <input
-                            name="fuzzy"
-                            type="checkbox"
-                            checked={this.state.fuzzy}
-                            onChange={this.handleCheckbox} 
-                        />
-                    </label>
+    const data = await fetch('/battles/_search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: cleanedReq
+    }).then(res => res.json()).catch(e => console.log(e))
+    this.props.fillResults(data)
+  }
 
-                    <label>
-                        <span className="describe"> Strength </span>
-                        <input
-                            className="range"
-                            name="strengthLow"
-                            type="number"
-                            value={this.state.strengthLow}
-                            onChange={this.handleInputChange} 
-                            placeholder="Low"
-                        />
-                        -
-                        <input
-                            className="range"
-                            name="strengthHigh"
-                            type="number"
-                            value={this.state.strengthHigh}
-                            onChange={this.handleInputChange} 
-                            placeholder="High"
-                        />
-                    </label>
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <span className="describe"> Search Query </span>
+            <input
+              name="query"
+              type="text"
+              value={this.state.query}
+              onChange={this.handleInputChange}
+              placeholder="Query text..."
+            />
+          </label>
 
-                    <label>
-                        <span className="describe"> Casualties </span>
-                        <input
-                            className="range"
-                            name="lossesLow"
-                            type="number"
-                            value={this.state.lossesLow}
-                            onChange={this.handleInputChange}
-                            placeholder="Low"
-                        />
-                        -
-                        <input
-                            className="range"
-                            name="lossesHigh"
-                            type="number"
-                            value={this.state.lossesHigh}
-                            onChange={this.handleInputChange} 
-                            placeholder="High"
-                        />
-                    </label>
+          <label>
+            <span className="describe"> Allow fuzzy search </span>
+            <input
+              name="fuzzy"
+              type="checkbox"
+              checked={this.state.fuzzy}
+              onChange={this.handleCheckbox}
+            />
+          </label>
 
-                    <label>
-                        <span className="describe"> Year </span>
+          <label>
+            <span className="describe"> Strength </span>
+            <input
+              className="range"
+              name="strengthLow"
+              type="number"
+              value={this.state.strengthLow}
+              onChange={this.handleInputChange}
+              placeholder="Low"
+            />
+            -
                         <input
-                            className="range"
-                            name="yearLow"
-                            type="number"
-                            value={this.state.yearLow}
-                            onChange={this.handleInputChange} 
-                            placeholder="Low (can be negative)"
-                        />
-                        -
+              className="range"
+              name="strengthHigh"
+              type="number"
+              value={this.state.strengthHigh}
+              onChange={this.handleInputChange}
+              placeholder="High"
+            />
+          </label>
+
+          <label>
+            <span className="describe"> Casualties </span>
+            <input
+              className="range"
+              name="lossesLow"
+              type="number"
+              value={this.state.lossesLow}
+              onChange={this.handleInputChange}
+              placeholder="Low"
+            />
+            -
                         <input
-                            className="range"
-                            name="yearHigh"
-                            type="number"
-                            value={this.state.yearHigh}
-                            onChange={this.handleInputChange} 
-                            placeholder="High (can be negative)"
-                        />
-                    </label>
+              className="range"
+              name="lossesHigh"
+              type="number"
+              value={this.state.lossesHigh}
+              onChange={this.handleInputChange}
+              placeholder="High"
+            />
+          </label>
 
-                    <label>
-                      <input 
-                        type="radio"
-                        name="sort"
-                        value="score"
-                        checked={this.state.sort === 'score'}
-                        onChange={this.handleInputChange} 
-                      />
-                      <span > Sort by score </span>
-                    </label>
+          <label>
+            <span className="describe"> Year </span>
+            <input
+              className="range"
+              name="yearLow"
+              type="number"
+              value={this.state.yearLow}
+              onChange={this.handleInputChange}
+              placeholder="Low (can be negative)"
+            />
+            -
+                        <input
+              className="range"
+              name="yearHigh"
+              type="number"
+              value={this.state.yearHigh}
+              onChange={this.handleInputChange}
+              placeholder="High (can be negative)"
+            />
+          </label>
 
-                    <label>
-                    <input 
-                        type="radio"
-                        name="sort"
-                        value="year"
-                        checked={this.state.sort === 'year'}
-                        onChange={this.handleInputChange} 
-                      />
-                      <span > Sort by year </span>
-                    </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              value="score"
+              checked={this.state.sort === 'score'}
+              onChange={this.handleInputChange}
+            />
+            <span > Sort by score </span>
+          </label>
 
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        )
-    }
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              value="year"
+              checked={this.state.sort === 'year'}
+              onChange={this.handleInputChange}
+            />
+            <span > Sort by year </span>
+          </label>
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchField;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -4,20 +4,28 @@ const formatYear = yearStr => {
     return yearStr && yearStr[0] === '-' ? `${yearStr.substring(1)} BC` : yearStr
 }
 
+const formatHighlight = highlight => {
+    return {__html: `Text: ${highlight.text}`}; 
+}
+
 function SearchResults (props) {
     const results = props.results && 
         props.results.map(result => {
             return <div className="result-row" key={result._id}>
                 <p className="result-title">Title: {result._source.title}</p>
-                <p>Result: {result._source.result}</p>
-                <p>Date: {result._source.date}</p>
-                <p>Location: {result._source.location}</p>
-                <p>Belligerents: {result._source.belligerents1} : {result._source.belligerents2}</p>
-                <p>Leaders: {result._source.leaders1} : {result._source.leaders2}</p>
-                <p>Strengths: {result._source.strength1} : {result._source.strength2}</p>
-                <p>Losses: {result._source.losses1} : {result._source.losses2}</p>
-                <p>Year: {formatYear(result._source.year)}</p>
-                <p>{JSON.stringify(result.highlight)}</p>
+                {result.highlight && <p dangerouslySetInnerHTML={formatHighlight(result.highlight)} />}
+                {result._source.result && <p>Result: {result._source.result}</p>}
+                {result._source.date && <p>Date: {result._source.date}</p>}
+                {result._source.location && <p>Location: {result._source.location}</p>}
+                {(result._source.belligerents1 || result._source.belligerents2)
+                    && <p>Belligerents: {result._source.belligerents1} : {result._source.belligerents2}</p>}
+                {(result._source.leaders1 || result._source.leaders2)
+                    && <p>Leaders: {result._source.leaders1} : {result._source.leaders2}</p>}
+                {(result._source.strength1 || result._source.strength2)
+                    && <p>Strengths: {result._source.strength1} : {result._source.strength2}</p>}
+                {(result._source.losses1 || result._source.losses2)
+                    && <p>Losses: {result._source.losses1} : {result._source.losses2}</p>}
+                {result._source.year && <p>Year: {formatYear(result._source.year)}</p>}
             </div>
         });
     return (
